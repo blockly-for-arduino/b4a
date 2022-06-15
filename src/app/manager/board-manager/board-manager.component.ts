0d8de103c4b206627499b1feb98bf90491ee986f
coloz <coloz999@gmail.com>
2022-06-16 00:07:16 +0800

    完善开发板管理

--- src/app/manager/board-manager/board-manager.component.ts
+++ src/app/manager/board-manager/board-manager.component.ts
@@ -12,9 +12,12 @@ export class BoardManagerComponent implements OnInit {
   @ViewChild('boardListBox', { static: false, read: ElementRef }) boardListBox: ElementRef
   boardManagerLoaded = false
 
-  viewMode = '1';
 
   boardList_cloud = []
+
+  viewMode = '1'; //1:按厂家查看；2:按核心查看；3:搜索模式
+  venderList = []
+  coreList = []
 
   get boardList() {
     return this.configService.boardList
@@ -26,12 +29,13 @@ export class BoardManagerComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
+    this.getCloudData()
   }
 
-  getData() {
+  getCloudData() {
     this.cloudService.getBoards().subscribe((resp: any) => {
       this.boardList_cloud = resp.data
+      this.viewModeChange()
     })
   }
 
@@ -45,6 +49,51 @@ export class BoardManagerComponent implements OnInit {
         localStorage.setItem('libList', JSON.stringify(sortable.toArray()))
       }
     })
+  }
+
+  viewModeChange() {
+    if (this.viewMode == '1')
+      this.change2VenderMode()
+    else
+      this.change2CoreMode()
+  }
+
+  change2VenderMode() {
+    let venderDict = {}
+    this.venderList = []
+    this.boardList_cloud.map(board => {
+      if (venderDict[board.vender]) {
+        venderDict[board.vender].boards.push(board)
+      } else {
+        let verderInfo = {
+          name: board.vender,
+          img: "",
+          boards: [board]
+        }
+        this.venderList.push(verderInfo)
+        venderDict[board.vender] = verderInfo
+      }
+    })
+    console.log(this.venderList);
+  }
+
+  change2CoreMode() {
+    let coreDict = {}
+    this.coreList = []
+    this.boardList_cloud.map(board => {
+      if (coreDict[board.core]) {
+        coreDict[board.core].boards.push(board)
+      } else {
+        let coreInfo = {
+          name: board.core,
+          img: "",
+          boards: [board]
+        }
+        this.coreList.push(coreInfo)
+        coreDict[board.core] = coreInfo
+      }
+    })
+    console.log(this.coreList);
   }
 
 }
