908e1163f38551a967c431350e2d986277baf571
downey <853384781@qq.com>
2022-06-15 16:04:28 +0800

    更新

--- src/app/blockly/blockly.component.ts
+++ src/app/blockly/blockly.component.ts
@@ -247,7 +247,7 @@ export class BlocklyComponent implements OnInit {
       const options = [];
       for (let i = 0; i < variableModelList.length; i++) {
         // Set the UUID as the internal representation of the variable.
-        if (!variableModelList[i].type || (TYPE && TYPE === variableModelList[i].type)) options.push([variableModelList[i].name, variableModelList[i].getId()]);
+        if (!TYPE || (TYPE && (!variableModelList[i].type || TYPE === variableModelList[i].type))) options.push([variableModelList[i].name, variableModelList[i].getId()]);
       }
       options.push([Blockly.Msg['RENAME_VARIABLE'], 'RENAME_VARIABLE_ID']);
       if (Blockly.Msg['DELETE_VARIABLE']) {
