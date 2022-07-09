79452928480619f7b9eba8438aaecf16145cf36a
coloz <coloz999@gmail.com>
2022-07-09 17:17:33 +0800

    git\7z 调整测试

---
+++ src/app/manager/board-manager/install-shell/git.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const git = require('isomorphic-git')
+const http = require('isomorphic-git/http/node')
+const fs = require('fs')
+
+const dir = path.join(process.cwd(), 'test-clone')
+git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs' }).then(console.log)
